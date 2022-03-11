c4851a1f3f6671c5f243ebb824bb4357abd46e9d
Gerard Mennella <gerardmennella21@gmail.com>
2022-03-11 18:03:49 -0500

    delete functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEL = document.querySelector("#task-form");
 var taksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content")
 
 var taskFormHandler = function(event) {
 
@@ -81,6 +82,17 @@ var createTaskActions = function(taskId) {
     }
     return actionContainerEl
 }
+var taskButtonHandler = function(event){
+
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id")
+        deleteTask(taskId)
+    }
+}
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "'")
+    taskSelected.remove()
+}
 
 formEL.addEventListener("submit", taskFormHandler);
-
+pageContentEl.addEventListener("click", taskButtonHandler);
